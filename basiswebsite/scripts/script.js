d4e9f9abd75966dd01c6bbce6e8f083b19c0fcd7
AFFRANS <65559359+AFFRANS@users.noreply.github.com>
2020-12-16 19:55:31 +0100

    Add files via upload

--- basiswebsite/scripts/script.js
+++ basiswebsite/scripts/script.js
@@ -1,1 +1,23 @@
 // JavaScript Document
+
+/* klikken met de muis op de feedback buttons*/
+
+//var feedbackButton = document.querySelector("form h2");
+//
+//feedbackButton.addEventListener("click", formHeenEnWeer);
+//
+//function formHeenEnWeer() {
+//    var hetFormulier = document.querySelector("form");
+//    hetFormulier.classList.toggle("toonForm");
+//}
+//var formHeenEnWeer = document.querySelector("form h2");
+
+
+var menuButton = document.querySelector("button");
+
+menuButton.addEventListener("click", fromHeenEnWeer);
+
+function formHeenEnWeer() {
+    var hetFormulier = document.querySelector("form");
+    hetFormulier.classList.toggle("toonForm");
+}
