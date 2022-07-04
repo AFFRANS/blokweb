9ca08294220b3f829e55fb87d0c7f682e74f4251
AFFRANS <65559359+AFFRANS@users.noreply.github.com>
2022-07-04 23:36:37 +0200

    Add files via upload

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,107 +1,104 @@
-// library --> https://sortablejs.github.io/Sortable/
-// documentatie https://github.com/SortableJS/Sortable
+// de kaarten met de vissen - bovenaan 
+const theStapel = document.querySelector('.kaarten ul');
+const theCards = theStapel.querySelectorAll('.kaarten ul li');
+
+// de counter onderaan
+const theCounter = document.querySelector('.zwemmen h1 span');
+
+// de lege lijst onderaan
+const theList = document.querySelector('.zwemmen ul');
 
 
-
-
-
-/************************/
-/* DE LIJSTEN & KNOPJES */
-/************************/
-var favoLijst = document.querySelector("ul:first-of-type");
-var allesLijst = document.querySelector("ul:last-of-type");
-
-// dit is een array
-var alleVoegToeKnopjes = document.querySelectorAll("ul:last-of-type button");
-
-
-
-
-
-/**********************/
-/* DRAGGEN EN DROPPEN */
-/**********************/
-new Sortable(allesLijst, {
-    group: {
-        name: 'fotoLijstjes', // zelfde naam als bij favoLijstje
-        pull: 'clone' // er wordt een kopie gemaakt
-    },
-    sort: false, // de foto kunnen in de lijst zelf niet gesorteerd worden
-    forceFallback: true, // om meer controle te hebben over ghost en cursor
-    animation: 300, // een kleine animatie als een foto van plek wisselt
-    onStart: function (event) {
-        document.body.classList.add("dragging");
-    },
-    onEnd: function (event) {
-        document.body.classList.remove("dragging");
-    }
-});
-
-new Sortable(favoLijst, {
-    group: {
-        name: 'fotoLijstjes', // zelfde naam als bij allesLijstje
-        pull: 'false' // foto's kunnen niet uit de favo lijst gesleept worden
-    },
-    forceFallback: true, // om meer controle te hebben over ghost en cursor
-    animation: 300, // een kleine animatie als een foto van plek wisselt
-    onAdd: function (event) {
-        // als een foto naar de favo lijst wordt gesleept
-
-        // de orginele foto - de button weer naar kliks laten luisteren
-        event.clone.addEventListener("click", voegFotoToeAlsFavo);
-
-        // nieuwe foto - de button wisselen naar verwijderen
-        vanVoegToeNaarVerwijderButton(event.item);
-    }
+/*****************/
+/*     SWIPEN    */
+/*****************/
+// kaarten
+theCards.forEach( card => {	
+	// het kunnen swipen naar links en rechts
+	let cardHammer = new Hammer(card);
+	cardHammer.on("swipeleft", (e) => {
+		let card = e.target;
+		trashCard(card);
+	});
+	cardHammer.on("swiperight", (e) => {
+		let card = e.target;
+		likeCard(card);
+	});
 });
 
 
+/******************/
+/* LINKER en RECHTER PIJLEN */
+/******************/
 
+document.addEventListener("keydown", handleKeypresses);
 
+function handleKeypresses(e) {
+	
+	switch (e.code) {
+		case "ArrowLeft":
+			if(getBovensteCard()) {
+				trashCard(getBovensteCard());
+			}
+			break;
+		case "ArrowRight":
+			if(getBovensteCard()) {
+				likeCard(getBovensteCard());
+			}
+			break;
+	}
+}
 
-/*******************/
-/* KNOPJES KLIKKEN */
-/*******************/
-alleVoegToeKnopjes.forEach(eenKnopje => {
-    // elk knopje in de array luistert naar kliks
-    eenKnopje.addEventListener("click", voegFotoToeAlsFavo);
-});
-
-function voegFotoToeAlsFavo(event) {
-    var deButtonWaaropGekliktIs = this;
-    // opzoeken welke li bij de button hoort
-    var deLiWaarDeButtonInZit = deButtonWaaropGekliktIs.closest("li");
-
-    // de li klonen en achteraan toevoegen aan de favo lijst
-    var fotoKloon = deLiWaarDeButtonInZit.cloneNode(true);
-    favoLijst.appendChild(fotoKloon);
-    // nog wat dingetjes met de toegevoegde li/foto doen
-    vanVoegToeNaarVerwijderButton(fotoKloon);
+function getBovensteCard() {
+	let swippebleCards, bovensteCard;
+	swippebleCards = theStapel.querySelectorAll("li:not(.trash):not(.like)");
+	bovensteCard = swippebleCards[swippebleCards.length - 1];
+	return bovensteCard;
 }
 
 
-function vanVoegToeNaarVerwijderButton(deFoto) {
-    // zorgen dat de nieuwe foto in beeld scrollt
-    deFoto.scrollIntoView({behavior: "smooth", inline: "center"});
+/*********************/
+/* EVENTS AFHANDELEN */
+/*********************/
 
-    // button wijzigen naar verwijderen
-    // de voegToe button verwijderen
-    var deButtonBijDeFoto = deFoto.querySelector("button");
-    deButtonBijDeFoto.remove();
+function trashCard(card) { 
+	card.classList.add("trash");
+};
+	
+function likeCard(card) {
+	card.classList.add("like");
+	
 
-    // een nieuwe button aanmaken
-    var deVerwijderButton = document.createElement("button");
-    // die button laten luisteren naar kliks
-    deVerwijderButton.addEventListener("click", verwijderFoto);
-    // die button toevoegen aan de li
-    deFoto.appendChild(deVerwijderButton);
-}
+	let cardClone = card.cloneNode(true);
+	
+	
+	let trashButton = document.createElement("button");
+	trashButton.textContent = "";
+
+	trashButton.addEventListener("click", trashFavo);
+
+	cardClone.appendChild(trashButton);
+	
+	theList.appendChild(cardClone);
+	
+	
+	
+	// counter ophogen
+	let huidigeWaarde = parseInt(theCounter.textContent);
+	let nieuweWaarde = huidigeWaarde + 1;
+	theCounter.textContent = nieuweWaarde;
+};
 
 
-function verwijderFoto() {
-    var deButtonWaaropGekliktIs = this;
-    // opzoeken welke li bij de button hoort
-    var deLiWaarDeButtonInZit = deButtonWaaropGekliktIs.closest("li");
-    // weg der mee - hatseflats
-    deLiWaarDeButtonInZit.remove();
+function trashFavo(e) {
+	let theButton = e.target;
+	let theFavoCard = theButton.closest("li");
+	theFavoCard.remove();
+	
+	
+	
+	// counter verlagen
+	let huidigeWaarde = parseInt(theCounter.textContent);
+	let nieuweWaarde = huidigeWaarde - 1;
+	theCounter.textContent = nieuweWaarde;
 }
